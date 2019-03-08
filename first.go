563b7d632f8a31c34e8950dd14ad00f8bf072420
zhouboxi <zhoubox@topband.com.cn>
2019-03-08 14:00:13 +0800

    第一次提交

---
+++ first.go
@@ -0,0 +1,15 @@
+//程序所属的包
+package main
+//导入依赖包
+import "fmt"
+//定义常量
+const NAME = "zhouboxi"
+//定义变量
+var mainName = "main name"
+
+func main() {
+	fmt.Println("hello")
+	fmt.Println(NAME)
+	fmt.Println(mainName)
+
+}
