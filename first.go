2731b9916c21ea3237d59cae39bd473706f7679c
zhouboxi <zhoubox@topband.com.cn>
2019-03-11 15:21:25 +0800

    基本结构学习

--- first.go
+++ first.go
@@ -3,12 +3,26 @@ package main
 //导入依赖包
 import "fmt"
 //定义常量
-const NAME = "zhouboxi"
-//定义变量
-var mainName = "main name"
+const NAME  string= "zhouboxi"
+//定义全局变量
+var mainName string= "main name"
+//一般类型声明
+type boboInt int
+//结构的声明
+type Learn struct {
 
+}
+//接口声明
+type Ilearn interface {
+
+}
+//函数第一
+func learnMore(){
+	fmt.Print("hello learnMore")
+}
+//main函数
 func main() {
-	fmt.Println("hello")
+	fmt.Println("hello main")
 	fmt.Println(NAME)
 	fmt.Println(mainName)
 
